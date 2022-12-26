a952d3078c4bd8d7071d2b4992a072f2c6f465a3
Mohamad Fajar Hafidh <mfajarhafidh9@gmail.com>
2022-12-26 23:32:11 +0700

    changing var to val for class call

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,14 +1,19 @@
 import gamesuit.GameSuit
 
 fun main(args: Array<String>){
-    var suit = GameSuit()
+
+    val suit = GameSuit()
+
     println("=================")
     println("GAME SUIT")
     println("=================")
+
     println("Silahkan ikuti seperti berikut : ")
     println("Gunting, Kertas, dan Batu")
+
     println("Masukkan pemain 1 : ")
     val player1 = readLine()!!.toString()
+
     println("Masukkan pemain 2 : ")
     val player2 = readLine()!!.toString()
 
